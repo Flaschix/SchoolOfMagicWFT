233910e1f275041c9137c42fb68fc69b13773e1c
Flaschix <mishasigalov1@gmail.com>
2024-07-31 18:44:10 +0300

    test new Movement7

--- server.js
+++ server.js
@@ -1,201 +1,185 @@
-const cluster = require('cluster');
-const os = require('os');
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const redis = require('redis');
 const { v4: uuidv4 } = require('uuid');
 
-const numCPUs = os.cpus().length;
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379'
+});
 
-if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
+redisClient.connect().catch(console.error);
 
-    // Fork workers.
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
+redisClient.on('error', (err) => {
+    console.log('Redis Client Error', err);
+});
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`);
-        cluster.fork(); // Restart the worker
-    });
-} else {
-    const redisClient = redis.createClient({
-        url: process.env.REDIS_URL || 'redis://localhost:6379'
-    });
+redisClient.on('connect', () => {
+    console.log('Connected to Redis');
+});
 
-    redisClient.connect().catch(console.error);
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-    redisClient.on('error', (err) => {
-        console.log('Redis Client Error', err);
-    });
+const PORT = process.env.PORT || 3000;
 
-    redisClient.on('connect', () => {
-        console.log('Connected to Redis');
-    });
+app.use(express.static(__dirname + '/public'));
 
-    const app = express();
-    const server = http.createServer(app);
-    const io = socketIo(server);
+const rooms = {};
 
-    const PORT = process.env.PORT || 3000;
-
-    app.use(express.static(__dirname + '/public'));
-
-    const rooms = {};
-
-    function generateRoomCode(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html');
-    });
-
-    io.on('connection', (socket) => {
-        console.log('New client connected');
-
-        socket.on('createRoom', async () => {
-            console.log('createRoom');
-            let preCode;
-            let check;
-
-            do {
-                preCode = `${generateRoomCode(100000, 999999)}`;
-                check = await redisClient.get(preCode);
-            } while (check != null);
-
-            const roomCode = preCode;
-            const roomId = uuidv4();
-
-            try {
-                await redisClient.set(roomCode, roomId, { EX: 400 }); // Устанавливаем срок действия 24 часа 86400
-                rooms[roomId] = { levels: {} };
-                socket.join(roomId);
-                socket.emit('roomCreated', roomCode);
-                console.log(`Room created with code: ${roomCode}`);
-            } catch (err) {
-                console.error('Error creating room:', err);
-            }
-        });
-
-        socket.on('checkRoom', async (roomCode) => {
-            try {
-                const roomId = await redisClient.get(roomCode);
-                if (!roomId) {
-                    socket.emit('roomNotFound');
-                    return;
-                } else {
-                    socket.emit('roomExists', roomCode);
-                    return;
-                }
-            } catch (err) {
-                console.error('Error checking room:', err);
-                socket.emit('error', 'An error occurred');
-            }
-        });
-
-        socket.on('joinRoom', async ({ roomCode, avatar, username }) => {
-            try {
-                const roomId = await redisClient.get(roomCode);
-                if (!roomId) {
-                    socket.emit('error', 'Room not found');
-                    return;
-                }
-
-                if (!rooms[roomId]) {
-                    rooms[roomId] = { levels: {} };
-                }
-
-                if (!rooms[roomId].levels['GameScene']) {
-                    rooms[roomId].levels['GameScene'] = {};
-                }
-
-                socket.roomId = roomId;
-                socket.currentLevel = 'GameScene';
-
-                socket.join(`${roomId}:${socket.currentLevel}`);
-
-                rooms[roomId].levels[socket.currentLevel][socket.id] = { id: socket.id, x: 1024, y: 1224, character: avatar, name: username, room: roomCode };
-
-                // Отправляем информацию о текущих игроках новому игроку
-                socket.emit('currentPlayers', rooms[roomId].levels[socket.currentLevel]);
-
-                // Уведомляем других игроков о новом игроке
-                socket.to(`${roomId}:${socket.currentLevel}`).emit(`newPlayer:${socket.currentLevel}`, rooms[roomId].levels[socket.currentLevel][socket.id]);
-
-                socket.on('disconnect', () => {
-                    console.log('Client disconnected');
-                    if (rooms[roomId]) {
-                        delete rooms[roomId].levels[socket.currentLevel][socket.id];
-                        io.to(`${roomId}:${socket.currentLevel}`).emit('playerDisconnected', socket.id);
-                    }
-                });
-
-                socket.on(`playerMovement:${socket.currentLevel}`, (movementData) => {
-                    if (rooms[roomId].levels[socket.currentLevel][socket.id]) {
-                        rooms[roomId].levels[socket.currentLevel][socket.id].x = movementData.x;
-                        rooms[roomId].levels[socket.currentLevel][socket.id].y = movementData.y;
-                        rooms[roomId].levels[socket.currentLevel][socket.id].velocityX = movementData.velocityX;
-                        rooms[roomId].levels[socket.currentLevel][socket.id].velocityY = movementData.velocityY;
-                        rooms[roomId].levels[socket.currentLevel][socket.id].isMoving = movementData.isMoving;
-                        rooms[roomId].levels[socket.currentLevel][socket.id].direction = movementData.direction;
-                        io.to(`${roomId}:${socket.currentLevel}`).emit(`playerMoved:${socket.currentLevel}`, { id: socket.id, ...movementData });
-                    }
-                });
-
-                socket.on('switchScene', (newScene, posX, posY) => {
-                    let avatarCur = rooms[roomId].levels[socket.currentLevel][socket.id].character;
-                    let usernameCur = rooms[roomId].levels[socket.currentLevel][socket.id].name;
-                    //выход с текущего уровня
-                    if (rooms[roomId].levels[socket.currentLevel]) {
-                        delete rooms[roomId].levels[socket.currentLevel][socket.id];
-                        io.to(`${roomId}:${socket.currentLevel}`).emit('playerDisconnected', socket.id);
-                        socket.leave(`${roomId}:${socket.currentLevel}`);
-                    }
-
-                    socket.removeAllListeners(`playerMovement:${socket.currentLevel}`);
-                    socket.currentLevel = newScene;
-
-                    if (!rooms[roomId].levels[newScene]) {
-                        rooms[roomId].levels[newScene] = {};
-                    }
-
-                    rooms[roomId].levels[newScene][socket.id] = { id: socket.id, x: posX, y: posY, character: avatarCur, name: usernameCur, room: roomCode };
-                    socket.join(`${roomId}:${newScene}`);
-
-                    socket.emit('sceneSwitched', { players: rooms[roomId].levels[newScene], scene: newScene });
-
-                    socket.to(`${roomId}:${newScene}`).emit(`newPlayer:${newScene}`, rooms[roomId].levels[newScene][socket.id]);
-
-                    // Переподключаем слушатель для новой сцены
-                    socket.on(`playerMovement:${newScene}`, (movementData) => {
-                        if (rooms[roomId].levels[newScene][socket.id]) {
-                            rooms[roomId].levels[newScene][socket.id].x = movementData.x;
-                            rooms[roomId].levels[newScene][socket.id].y = movementData.y;
-                            rooms[roomId].levels[newScene][socket.id].velocityX = movementData.velocityX;
-                            rooms[roomId].levels[newScene][socket.id].velocityY = movementData.velocityY;
-                            io.to(`${roomId}:${newScene}`).emit(`playerMoved:${newScene}`, { id: socket.id, ...movementData });
-                        }
-                    });
-
-                });
-
-                socket.on('playerReconnect', (newSettings) => {
-                    if (rooms[roomId].levels[socket.currentLevel][socket.id]) {
-                        io.to(`${roomId}:${socket.currentLevel}`).emit('playerDisconnected', socket.id);
-
-                        rooms[roomId].levels[socket.currentLevel][socket.id] = { id: socket.id, x: newSettings.x, y: newSettings.y, character: newSettings.avatar, name: newSettings.name, room: roomCode };
-                        socket.to(`${roomId}:${socket.currentLevel}`).emit(`newPlayer:${socket.currentLevel}`, rooms[roomId].levels[socket.currentLevel][socket.id]);
-                    }
-                });
-            } catch (err) {
-                console.error('Error joining room:', err);
-                socket.emit('error', 'An error occurred');
-            }
-        });
-    });
-
-    server.listen(PORT, () => console.log(`Server running on port ${PORT} and worker ${process.pid}`));
+function generateRoomCode(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('createRoom', async () => {
+        console.log('createRoom');
+        let preCode;
+        let check
+
+        do {
+            preCode = `${generateRoomCode(100000, 999999)}`
+            check = await redisClient.get(preCode);
+        } while (check != null)
+
+        const roomCode = preCode;
+        const roomId = uuidv4();
+
+        try {
+            await redisClient.set(roomCode, roomId, { EX: 400 }); // Устанавливаем срок действия 24 часа 86400
+            rooms[roomId] = { levels: {} };
+            socket.join(roomId);
+            socket.emit('roomCreated', roomCode);
+            console.log(`Room created with code: ${roomCode}`);
+        } catch (err) {
+            console.error('Error creating room:', err);
+        }
+    });
+
+    socket.on('checkRoom', async (roomCode) => {
+        try {
+            const roomId = await redisClient.get(roomCode);
+            if (!roomId) {
+                socket.emit('roomNotFound');
+                return;
+            } else {
+                socket.emit('roomExists', roomCode);
+                return;
+            }
+        } catch (err) {
+            console.error('Error checking room:', err);
+            socket.emit('error', 'An error occurred');
+        }
+    });
+
+    socket.on('joinRoom', async ({ roomCode, avatar, username }) => {
+        try {
+            const roomId = await redisClient.get(roomCode);
+            if (!roomId) {
+                socket.emit('error', 'Room not found');
+                return;
+            }
+
+            if (!rooms[roomId]) {
+                rooms[roomId] = { levels: {} };
+            }
+
+            if (!rooms[roomId].levels['GameScene']) {
+                rooms[roomId].levels['GameScene'] = {};
+            }
+
+            // socket.join(roomId);
+            socket.roomId = roomId;
+            socket.currentLevel = 'GameScene';
+
+            socket.join(`${roomId}:${socket.currentLevel}`);
+
+            rooms[roomId].levels[socket.currentLevel][socket.id] = { id: socket.id, x: 1024, y: 1224, character: avatar, name: username, room: roomCode };
+
+            // Отправляем информацию о текущих игроках новому игроку
+            socket.emit('currentPlayers', rooms[roomId].levels[socket.currentLevel]);
+
+            // Уведомляем других игроков о новом игроке
+            socket.to(`${roomId}:${socket.currentLevel}`).emit(`newPlayer:${socket.currentLevel}`, rooms[roomId].levels[socket.currentLevel][socket.id]);
+
+            socket.on(`disconnect`, () => {
+                console.log('Client disconnected');
+                if (rooms[roomId]) {
+                    delete rooms[roomId].levels[socket.currentLevel][socket.id];
+                    io.to(`${roomId}:${socket.currentLevel}`).emit('playerDisconnected', socket.id);
+                }
+            });
+
+            socket.on(`playerMovement:${socket.currentLevel}`, (movementData) => {
+                if (rooms[roomId].levels[socket.currentLevel][socket.id]) {
+                    rooms[roomId].levels[socket.currentLevel][socket.id].x = movementData.x;
+                    rooms[roomId].levels[socket.currentLevel][socket.id].y = movementData.y;
+                    rooms[roomId].levels[socket.currentLevel][socket.id].velocityX = movementData.velocityX;
+                    rooms[roomId].levels[socket.currentLevel][socket.id].velocityY = movementData.velocityY;
+                    rooms[roomId].levels[socket.currentLevel][socket.id].isMoving = movementData.isMoving;
+                    rooms[roomId].levels[socket.currentLevel][socket.id].direction = movementData.direction;
+                    io.to(`${roomId}:${socket.currentLevel}`).emit(`playerMoved:${socket.currentLevel}`, { id: socket.id, ...movementData });
+                }
+            });
+
+
+            socket.on('switchScene', (newScene, posX, posY) => {
+                let avatarCur = rooms[roomId].levels[socket.currentLevel][socket.id].character;
+                let usernameCur = rooms[roomId].levels[socket.currentLevel][socket.id].name;
+                //выход с текущего уровня
+                if (rooms[roomId].levels[socket.currentLevel]) {
+                    delete rooms[roomId].levels[socket.currentLevel][socket.id];
+                    io.to(`${roomId}:${socket.currentLevel}`).emit('playerDisconnected', socket.id);
+                    socket.leave(`${roomId}:${socket.currentLevel}`);
+                }
+
+                socket.removeAllListeners(`playerMovement:${socket.currentLevel}`);
+                socket.currentLevel = newScene;
+
+                if (!rooms[roomId].levels[newScene]) {
+                    rooms[roomId].levels[newScene] = {};
+                }
+
+                rooms[roomId].levels[newScene][socket.id] = { id: socket.id, x: posX, y: posY, character: avatarCur, name: usernameCur, room: roomCode };
+                socket.join(`${roomId}:${newScene}`);
+
+                socket.emit('sceneSwitched', { players: rooms[roomId].levels[newScene], scene: newScene });
+
+                socket.to(`${roomId}:${newScene}`).emit(`newPlayer:${newScene}`, rooms[roomId].levels[newScene][socket.id]);
+
+                // Переподключаем слушатель для новой сцены
+                socket.on(`playerMovement:${newScene}`, (movementData) => {
+                    if (rooms[roomId].levels[newScene][socket.id]) {
+                        rooms[roomId].levels[newScene][socket.id].x = movementData.x;
+                        rooms[roomId].levels[newScene][socket.id].y = movementData.y;
+                        rooms[roomId].levels[newScene][socket.id].velocityX = movementData.velocityX;
+                        rooms[roomId].levels[newScene][socket.id].velocityY = movementData.velocityY;
+                        io.to(`${roomId}:${newScene}`).emit(`playerMoved:${newScene}`, { id: socket.id, ...movementData });
+                    }
+                });
+
+            });
+
+            socket.on('playerReconnect', (newSettings) => {
+                if (rooms[roomId].levels[socket.currentLevel][socket.id]) {
+                    io.to(`${roomId}:${socket.currentLevel}`).emit('playerDisconnected', socket.id);
+
+                    rooms[roomId].levels[socket.currentLevel][socket.id] = { id: socket.id, x: newSettings.x, y: newSettings.y, character: newSettings.avatar, name: newSettings.name, room: roomCode };
+                    socket.to(`${roomId}:${socket.currentLevel}`).emit(`newPlayer:${socket.currentLevel}`, rooms[roomId].levels[socket.currentLevel][socket.id]);
+                }
+            })
+        } catch (err) {
+            console.error('Error joining room:', err);
+            socket.emit('error', 'An error occurred');
+        }
+    });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
