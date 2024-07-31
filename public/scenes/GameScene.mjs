f34ac9f844523853cdabdd454d1e3c7b2bba3865
Flaschix <mishasigalov1@gmail.com>
2024-07-31 17:51:38 +0300

    test new Movement2

--- public/scenes/GameScene.mjs
+++ public/scenes/GameScene.mjs
@@ -216,7 +216,7 @@ export class GameScene extends Phaser.Scene {
                 targets: player,
                 x: playerInfo.x,
                 y: playerInfo.y,
-                duration: 50,
+                duration: 100,
                 onUpdate: function () {
                     // Обновление анимации на основе данных о движении
                     self.playersController.updateAnimOtherPlayer(player, {
