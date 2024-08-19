c4b42205aa59d72c7c72fe7b0a16298f6a29476b
Flaschix <mishasigalov1@gmail.com>
2024-08-19 12:26:56 +0300

    add 24h

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ io.on('connection', (socket) => {
         const roomId = uuidv4();
 
         try {
-            await redisClient.set(roomCode, roomId, { EX: 400 }); // Устанавливаем срок действия 24 часа 86400
+            await redisClient.set(roomCode, roomId, { EX: 86400 }); // Устанавливаем срок действия 24 часа 86400
             rooms[roomId] = { levels: {} };
             socket.join(roomId);
             socket.emit('roomCreated', roomCode);
